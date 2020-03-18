1ef1f30f7e815cf2d9defd8faeaa0b05c219325d
Theo Ribeiro <theosribeiro@gmail.com>
2020-03-18 17:48:48 -0300

    BD usando knex 2 - inserindo tables

--- backend/knowledge/versao-inicial/backend/config/db.js
+++ backend/knowledge/versao-inicial/backend/config/db.js
@@ -1,5 +1,7 @@
 const config = require('../knexfile')
 const knex = require('knex')(config) // ja passa o arquivo de configuracao para o knex
 
+
+knex.migrate.latest([config]) // no momento que carregar o back, ele passa a configuracao para fazer o migrate
 //exportar o knex para usar no arquivo index.js
 module.exports = knex
--- backend/knowledge/versao-inicial/backend/migrations/20200318162507_create_table_users.js
+++ backend/knowledge/versao-inicial/backend/migrations/20200318162507_create_table_users.js
@@ -1,8 +1,16 @@
-
+//up serve para evoluir o DB - criar/excluir tabela, alterar coluna... ir para as versoes mais novas
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('users', table => { //criar todos os campos da tabela
+        table.increments('id').primary()
+        table.string('name').notNull()
+        table.string('email').notNull().unique()
+        table.string('password').notNull()
+        table.string('admin').notNull().defaultTo(false)
+
+  })
 };
 
+//down server para desfazer uma versao
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('users')
 };
