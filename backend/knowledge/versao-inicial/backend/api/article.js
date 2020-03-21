d58dd10a1f63bf6769e739d3ff0e7ac526af3855
Theo Ribeiro <theosribeiro@gmail.com>
2020-03-21 17:20:09 -0300

    API de Artigo 2 - queries

--- backend/knowledge/versao-inicial/backend/api/article.js
+++ backend/knowledge/versao-inicial/backend/api/article.js
@@ -1,3 +1,5 @@
+const queries = require('./queries')
+
 module.exports = app => {
     const {existsOrError} = app.api.validation
 
@@ -72,6 +74,23 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
-    return {save, remove, get, getById}
+    
+    const getByCategory = async (req, res) => {
+        const categoryId = req.params.id //categoria pai
+        const page = req.query.page || 1
+        const categories = await app.db.raw(queries.categoryWithChildren, categoryId)
+        const ids = categories.rows.map(c => c.id) //ids filhas
+
+        app.db({ a: 'articles', u: 'users' })
+            .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name' })
+            .limit(limit).offset(page * limit - limit)
+            .whereRaw('?? = ??', ['u.id', 'a.userId']) //pegar o userId de articles e q seja o mesmo de user
+            .whereIn('categoryId', ids) //buscar somente os ids que sao filhas desse pai
+            .orderBy('a.id', 'desc')
+            .then(articles => res.json(articles))
+            .catch(err => res.status(500).send(err))
+    }
+
+    return {save, remove, get, getById, getByCategory}
 }
 
--- backend/knowledge/versao-inicial/backend/config/routes.js
+++ backend/knowledge/versao-inicial/backend/config/routes.js
@@ -29,4 +29,7 @@ module.exports = app => { //definir as rotas da aplicacao
         .get(app.api.article.getById)
         .put(app.api.article.save)
         .delete(app.api.article.remove)
+
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory)
 }
