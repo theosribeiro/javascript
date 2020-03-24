6c470aa1773cd3cec8b292c611ba85055c6ae541
Theo Ribeiro <theosribeiro@gmail.com>
2020-03-24 13:35:35 -0300

    Integrando Bancos com Scheduler - MonogoDB e Postgres

--- backend/knowledge/versao-inicial/backend/index.js
+++ backend/knowledge/versao-inicial/backend/index.js
@@ -13,6 +13,7 @@ consign()
     .then('./config/middlewares.js') //vai ser carregado esse arquivo pelo consign
     .then('./api/validation.js')//tem q carregar o validation antes da api do user
     .then('./api') // vai ser carregado todos os arquivos da pasta api pelo consign
+    .then('./schedule') //carregar todos os arquivos dentro da pasta schedule
     .then('./config/routes.js') //so le as rotas depois de carregar a api para ter acesso
     .into(app) //injetar em cada dependencia como parametro o app criado
 
