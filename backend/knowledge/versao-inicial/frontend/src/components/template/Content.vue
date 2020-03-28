3c62e94d3430657fa2d4d1efec5afac86c26cfb3
Theo Ribeiro <theosribeiro@gmail.com>
2020-03-28 19:52:55 -0300

    Front End - Estrutura inicial do Template

---
+++ backend/knowledge/versao-inicial/frontend/src/components/template/Content.vue
@@ -0,0 +1,18 @@
+
+<template> 
+    <div class="content"></div>
+</template>
+
+<script>
+export default {
+    name: 'Content'
+}
+</script>
+
+<style>
+    .content {
+        grid-area: content;
+        background-color: #eeffee;
+        padding: 20px;
+    }
+</style>
